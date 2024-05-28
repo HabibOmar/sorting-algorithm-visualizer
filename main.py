82a9f3b779f676c799abec05312a50b79557378f
Omar Habib <omar1farouk@gmail.com>
2024-05-28 20:01:42 +0300

    Implemented Pygame Window

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
 import random
+
 pygame.init()
 
-class Drawer:
+
+class DrawInformation:
     BLACK = 0, 0, 0
     WHITE = 255, 255, 255
     GREEN = 0, 255, 0
     RED = 255, 0, 0
     GREY = 128, 128, 128
     BACKGROUND_COLOR = WHITE
+
+    SIDE_PAD = 100
+    TOP_PAD = 150
 
     def __init__(self, width, height, lst):
         self.width = width
@@ -18,6 +23,47 @@ class Drawer:
         pygame.display.set_caption("Sorting Algorithm Visualizer")
         self.set_list(lst)
 
-        def set_list(self, lst):
-            pass
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
 
+        self.block_width = round((self.width - self.SIDE_PAD) / len(lst))
+        self.block_height = round((self.height - self.TOP_PAD) / (self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
+
+
+def generate_starting_list(n, min_val, max_val):
+    lst = []
+
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        lst.append(val)
+    return lst
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    n = 50
+    min_val = 0
+    max_val = 100
+
+    lst = generate_starting_list(n, min_val, max_val)
+    draw_info = DrawInformation(800, 600, lst)
+
+    while run:
+        clock.tick(60)
+
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main()
