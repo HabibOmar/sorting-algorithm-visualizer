bfb92958020fc46772c7552d89f471bd8a579ddb
Omar Habib <omar1farouk@gmail.com>
2024-05-28 19:35:37 +0300

    First Commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import pygame
+import random
+pygame.init()
+
+class Drawer:
+    BLACK = 0, 0, 0
+    WHITE = 255, 255, 255
+    GREEN = 0, 255, 0
+    RED = 255, 0, 0
+    GREY = 128, 128, 128
+    BACKGROUND_COLOR = WHITE
+
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visualizer")
+        self.set_list(lst)
+
+        def set_list(self, lst):
+            pass
+
